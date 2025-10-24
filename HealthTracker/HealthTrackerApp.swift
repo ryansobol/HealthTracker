e42456f41dd315164642935457c01bb3630662cc
Ryan Sobol <hello@ryansobol.com>
2025-10-24 12:19:44 -0700

    Initial Commit

---
+++ HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Ryan Sobol on 10/24/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthTrackerTests/HealthTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerTests.swift
+//  HealthTrackerTests
+//
+//  Created by Ryan Sobol on 10/24/25.
+//
+
+import Testing
+@testable import HealthTracker
+
+struct HealthTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
