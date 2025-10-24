f8cc3ba9d8481a9ac3ed0e7521f57b38b5b264e2
Ryan Sobol <hello@ryansobol.com>
2025-10-24 13:38:09 -0700

    Initial navigation setup

--- HealthTracker/ContentView.swift
+++ HealthTracker/ContentView.swift
@@ -1,27 +1,60 @@
 import SwiftUI
 
+enum HealthMetricContext: CaseIterable, Identifiable {
+	case steps
+	case weight
+
+	var id: Self {
+		return self
+	}
+
+	var title: String {
+		return switch self {
+		case .steps: "Steps"
+		case .weight: "Weight"
+		}
+	}
+
+	var tint: Color {
+		return switch self {
+		case .steps: .pink
+		case .weight: .indigo
+		}
+	}
+}
+
 struct ContentView: View {
+	@State private var selectedStat = HealthMetricContext.steps
+
 	var body: some View {
 		NavigationStack {
 			ScrollView {
-				VStack(spacing: 14) {
-					VStack {
-						HStack {
-							VStack(alignment: .leading) {
-								Label("Steps", systemImage: "figure.walk")
-									.font(.title3.bold())
-									.foregroundStyle(.pink)
-
-								Text("Avg: 10K steps")
-									.font(.caption)
-									.foregroundStyle(.secondary)
-							}
-
-							Spacer()
-
-							Image(systemName: "chevron.right")
-								.foregroundStyle(.secondary)
+				VStack(spacing: 20) {
+					Picker("Selected Stat", selection: self.$selectedStat) {
+						ForEach(HealthMetricContext.allCases) { metric in
+							Text(metric.title)
 						}
+					}
+					.pickerStyle(.segmented)
+
+					VStack {
+						NavigationLink(value: self.selectedStat) {
+							HStack {
+								VStack(alignment: .leading) {
+									Label("Steps", systemImage: "figure.walk")
+										.font(.title3.bold())
+										.foregroundStyle(.pink)
+
+									Text("Avg: 10K steps")
+										.font(.caption)
+								}
+
+								Spacer()
+
+								Image(systemName: "chevron.right")
+							}
+						}
+						.foregroundStyle(.secondary)
 						.padding(.bottom, 12)
 
 						RoundedRectangle(cornerRadius: 12)
@@ -58,7 +91,11 @@ struct ContentView: View {
 			}
 			.padding()
 			.navigationTitle("Dashboard")
+			.navigationDestination(for: HealthMetricContext.self) { metric in
+				Text(metric.title)
+			}
 		}
+		.tint(self.selectedStat.tint)
 	}
 }
 
