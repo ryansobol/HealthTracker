6e49abf92de9f521db05d34164ac19a6ad353bf7
Ryan Sobol <hello@ryansobol.com>
2025-10-27 09:41:23 -0700

    Add average step count per weekday interactivity

--- HealthTracker/Charts/StepPieChart.swift
+++ HealthTracker/Charts/StepPieChart.swift
@@ -4,6 +4,22 @@ import SwiftUI
 struct StepPieChart: View {
 	@Environment(HealthKitManager.self) private var healthKitManager
 
+	@State private var rawSelectedChartValue: Double? = 0
+
+	var selectedWeekday: WeekdayChartData? {
+		guard let rawSelectedChartValue = self.rawSelectedChartValue else {
+			return nil
+		}
+
+		var total = 0.0
+
+		return ChartMath.averageWeekdayCount(for: self.healthKitManager.stepData).first { weekday in
+			total += weekday.value
+
+			return rawSelectedChartValue <= total
+		}
+	}
+
 	var body: some View {
 		VStack(alignment: .leading) {
 			VStack(alignment: .leading) {
@@ -21,13 +37,30 @@ struct StepPieChart: View {
 					SectorMark(
 						angle: .value("Average Steps", weekday.value),
 						innerRadius: .ratio(0.618),
+						outerRadius: self.selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
 						angularInset: 1,
 					)
 					.foregroundStyle(.pink.gradient)
 					.cornerRadius(6)
+					.opacity(self.selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1 : 0.3)
 				}
 			}
 			.frame(height: 240)
+			.chartAngleSelection(value: self.$rawSelectedChartValue.animation(.smooth(duration: 0.25)))
+			.chartBackground { _ in
+				if let selectedWeekday = self.selectedWeekday {
+					VStack {
+						Text(selectedWeekday.date.weekdayTitle)
+							.font(.title2.bold())
+							.animation(.none, value: selectedWeekday.date.weekdayTitle)
+
+						Text(selectedWeekday.value, format: .number.precision(.fractionLength(0)))
+							.fontWeight(.medium)
+							.foregroundStyle(.secondary)
+							.contentTransition(.numericText())
+					}
+				}
+			}
 		}
 		.padding()
 		.background {
