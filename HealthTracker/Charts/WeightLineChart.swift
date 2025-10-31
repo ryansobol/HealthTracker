cc8ceeeb8922d28b5e3c3e58172b868bfb8b7cca
Ryan Sobol <hello@ryansobol.com>
2025-10-31 09:57:05 -0700

    Custom rule and axis marks for the average weight chart

--- HealthTracker/Charts/WeightLineChart.swift
+++ HealthTracker/Charts/WeightLineChart.swift
@@ -4,7 +4,13 @@ import SwiftUI
 struct WeightLineChart: View {
 	@Environment(HealthKitManager.self) private var healthKitManager
 
+	let goal = 165
+
 	let selectedStat: HealthMetricContext
+
+	var minValue: Double {
+		return self.healthKitManager.weightData.map {$0.value }.min() ?? 0
+	}
 
 	var body: some View {
 		VStack {
@@ -13,7 +19,7 @@ struct WeightLineChart: View {
 					VStack(alignment: .leading) {
 						Label("Weight", systemImage: "figure")
 							.font(.title3.bold())
-							.foregroundStyle(.indigo)
+							.foregroundStyle(self.selectedStat.tint)
 
 						Text("Avg: 180 lbs")
 							.font(.caption)
@@ -28,20 +34,42 @@ struct WeightLineChart: View {
 			.padding(.bottom, 12)
 
 			Chart {
+				RuleMark(y: .value("Goal", self.goal))
+					.foregroundStyle(.mint)
+					.lineStyle(.init(lineWidth: 1, dash: [5]))
+
 				ForEach(self.healthKitManager.weightData) { weight in
 					AreaMark(
 						x: .value("Day", weight.date, unit: .day),
-						y: .value("Value", weight.value),
+						yStart: .value("Value", weight.value),
+						yEnd: .value("Min value", self.minValue),
 					)
-					.foregroundStyle(Gradient(colors: [.blue.opacity(0.5), .clear]))
+					.foregroundStyle(Gradient(colors: [self.selectedStat.tint.opacity(0.5), .clear]))
 
 					LineMark(
 						x: .value("Day", weight.date, unit: .day),
 						y: .value("Value", weight.value),
 					)
+					.foregroundStyle(self.selectedStat.tint)
+					.symbol(.circle)
 				}
+				.interpolationMethod(.catmullRom)
 			}
 			.frame(height: 150)
+			.chartYScale(domain: .automatic(includesZero: false))
+			.chartXAxis {
+				AxisMarks { _ in
+					AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+				}
+			}
+			.chartYAxis {
+				AxisMarks { value in
+					AxisGridLine()
+						.foregroundStyle(.gray.opacity(0.3))
+
+					AxisValueLabel()
+				}
+			}
 		}
 		.padding()
 		.background {
@@ -54,7 +82,7 @@ struct WeightLineChart: View {
 #Preview {
 	@Previewable @State var healthKitManager = HealthKitManager()
 
-	WeightLineChart(selectedStat: .steps)
+	WeightLineChart(selectedStat: .weight)
 		.task {
 			try! await healthKitManager.fetchData()
 		}
